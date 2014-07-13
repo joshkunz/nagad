397bbeb16324dd22679945d562efabe6dc9409ef
Josh Kunz <joshkunz@me.com>
2014-07-13 07:55:39 -0600

    Adds the three basic endpoints.

--- server/Nagad.ml
+++ server/Nagad.ml
@@ -8,12 +8,29 @@ open HTTP
 open Thread
 open Mutex
 
+(* Close the connection that backs the given streams *)
+let terminate (ic, oc) =
+    Unix.shutdown (descr_of_out_channel oc) SHUTDOWN_ALL;
+    close_in_noerr ic;
+    close_out_noerr oc;;
+
 let handle_client (ic, oc, addr) = 
-    print_endline "Got new client!";;
+    let open Request in 
+    let request = Request.read ic in
+    match request.uri with
+    | "/facts" -> 
+        Response.make 200 "OK" "Facts." |> Response.write oc;
+    | "/graph" -> 
+        Response.make 200 "OK" "Graph." |> Response.write oc;
+    | "/query" -> 
+        Response.make 200 "OK" "Graph." |> Response.write oc;
+    | _ -> Response.make 404 "Not Found" "" |> Response.write oc;
+    terminate (ic, oc);;
 
 let main port =
     let tcp = (getprotobyname "tcp").p_proto in
     let sock = socket PF_INET SOCK_STREAM tcp in
+    setsockopt sock SO_REUSEADDR true;
     ADDR_INET (inet_addr_any, port) |> bind sock;
     listen sock 10;
 
@@ -24,4 +41,4 @@ let main port =
         accept_loop () in
     accept_loop ();;
 
-main 80;;
+main 8080;;
